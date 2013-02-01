ab4fced00197bba15397febbbf1f2ea6b23ab84d
Sallyca <catherine.vladimirskaya@gmail.com>
2013-02-01 10:20:30 +0400

    data cleaned

--- app/models/ithero.rb
+++ app/models/ithero.rb
@@ -31,6 +31,10 @@ class Ithero < ActiveRecord::Base
     where(:language => speciality).where('speciality like ?','%Software Engineer%').average(:netwage)
   end
 
+  def self.average_for_pos(position)
+    where(:speciality=>position).average(:netwage)
+  end
+
   def self.diff_for(speciality)
     where(:language => speciality).where('speciality like ?','%Software Engineer%').average(:wage_diff)
   end
